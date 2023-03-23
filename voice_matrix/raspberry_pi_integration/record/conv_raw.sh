6861a090baee1b480a095328e6dda812585e2c27
Trevor McDonald <mcdonald.tr@northeastern.edu>
2023-03-23 01:13:39 -0400

    Added examples including 8 channel record #2

---
+++ voice_matrix/raspberry_pi_integration/record/conv_raw.sh
@@ -0,0 +1,11 @@
+
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 mic_16000_raw_s16le_channel_0.raw channel_0.wav
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 mic_16000_raw_s16le_channel_1.raw channel_1.wav
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 mic_16000_raw_s16le_channel_2.raw channel_2.wav
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 mic_16000_raw_s16le_channel_3.raw channel_3.wav
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 mic_16000_raw_s16le_channel_4.raw channel_4.wav
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 mic_16000_raw_s16le_channel_5.raw channel_5.wav
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 mic_16000_raw_s16le_channel_6.raw channel_6.wav
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 mic_16000_raw_s16le_channel_7.raw channel_7.wav
+
+
