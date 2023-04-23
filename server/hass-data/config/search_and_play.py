b5454b9616b819cfd910bd5850fc6a55e71a00a2
Iris Wang <wang.ir@northeastern.edu>
2023-04-23 00:36:05 -0400

    Add missing files to automatically setup Spotify integration

---
+++ server/hass-data/config/search_and_play.py
@@ -0,0 +1,27 @@
+import spotipy
+import sys
+from spotipy.oauth2 import SpotifyOAuth
+import re
+
+query = ' '
+
+if len(sys.argv) > 1:
+    query = query.join(sys.argv[1:])
+else:
+    exit()
+
+query = re.search('search spotify for (.*)', query).group(1)
+
+print(query)
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth())
+
+res = sp.search(query)
+song = "spotify:track:" + res['tracks']['items'][0]['id']
+print(song)
+
+uris = [song]
+
+print(uris)
+
+sp.start_playback(uris=uris)
