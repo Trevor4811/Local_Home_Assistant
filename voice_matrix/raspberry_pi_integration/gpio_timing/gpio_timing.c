ac2c0c7e07c9c334c14defd4838891bae05fcf9c
Trevor McDonald <mcdonald.tr@northeastern.edu>
2023-04-27 09:43:32 -0400

    #27 pigpio example file timing program was based

--- voice_matrix/raspberry_pi_integration/gpio_timing/gpio_timing.c
+++ voice_matrix/raspberry_pi_integration/gpio_timing/gpio_timing.c
@@ -71,7 +71,7 @@ sudo ./freq1 4 6 -r10
 
 #define OPT_R_MIN 1
 #define OPT_R_MAX 300
-#define OPT_R_DEF 100
+#define OPT_R_DEF 10
 
 #define OPT_S_MIN 1
 #define OPT_S_MAX 10
@@ -94,23 +94,28 @@ static uint32_t g_mask;
 static int g_num_gpios;
 static int g_gpio[MAX_GPIOS];
 
-static int g_opt_p = OPT_P_DEF; //
-static int g_opt_r = OPT_R_DEF; // refresh rate
-static int g_opt_s = OPT_S_DEF; // sample rate
+static int g_opt_p = OPT_P_DEF;
+static int g_opt_r = OPT_R_DEF;
+static int g_opt_s = OPT_S_DEF;
 static int g_opt_t = 0;
 
 void usage()
 {
-   fprintf(stderr,
-           "\n"
-           "Usage: sudo ./freq_count_1 gpio ... [OPTION] ...\n"
-           "\nEXAMPLE\n"
-           "sudo ./gpio_timing 4\n"
-           "Monitor gpio 4\n"
-           "\n",
-           OPT_P_MIN, OPT_P_MAX,
-           OPT_R_MIN, OPT_R_MAX, OPT_R_DEF,
-           OPT_S_MIN, OPT_S_MAX, OPT_S_DEF);
+   fprintf
+   (stderr,
+      "\n" \
+      "Usage: sudo ./freq_count_1 gpio ... [OPTION] ...\n" \
+      "   -p value, sets pulses every p micros, %d-%d, TESTING only\n" \
+      "   -r value, sets refresh period in deciseconds, %d-%d, default %d\n" \
+      "   -s value, sets sampling rate in micros, %d-%d, default %d\n" \
+      "\nEXAMPLE\n" \
+      "sudo ./freq_count_1 4 7 -r2 -s2\n" \
+      "Monitor gpios 4 and 7.  Refresh every 0.2 seconds.  Sample rate 2 micros.\n" \
+      "\n",
+      OPT_P_MIN, OPT_P_MAX,
+      OPT_R_MIN, OPT_R_MAX, OPT_R_DEF,
+      OPT_S_MIN, OPT_S_MAX, OPT_S_DEF
+   );
 }
 
 void fatal(int show_usage, char *fmt, ...)
@@ -124,29 +129,58 @@ void fatal(int show_usage, char *fmt, ...)
 
    fprintf(stderr, "%s\n", buf);
 
-   if (show_usage)
-      usage();
+   if (show_usage) usage();
 
    fflush(stderr);
 
    exit(EXIT_FAILURE);
 }
 
+static int initOpts(int argc, char *argv[])
+{
+   int i, opt;
+
+   while ((opt = getopt(argc, argv, "p:r:s:")) != -1)
+   {
+      i = -1;
+
+      switch (opt)
+      {
+         case 'p':
+            i = atoi(optarg);
+            if ((i >= OPT_P_MIN) && (i <= OPT_P_MAX))
+               g_opt_p = i;
+            else fatal(1, "invalid -p option (%d)", i);
+            g_opt_t = 1;
+            break;
+
+         case 'r':
+            i = atoi(optarg);
+            if ((i >= OPT_R_MIN) && (i <= OPT_R_MAX))
+               g_opt_r = i;
+            else fatal(1, "invalid -r option (%d)", i);
+            break;
+
+         case 's':
+            i = atoi(optarg);
+            if ((i >= OPT_S_MIN) && (i <= OPT_S_MAX))
+               g_opt_s = i;
+            else fatal(1, "invalid -s option (%d)", i);
+            break;
+
+        default: /* '?' */
+           usage();
+           exit(-1);
+        }
+    }
+   return optind;
+}
+
 void edges(int gpio, int level, uint32_t tick)
 {
+   l_gpio_data[gpio].last_tick = tick;
 
-   if (level == 0)
-   {
-      if (l_gpio_data[gpio].pulse_count == 3) {
-         l_gpio_data[gpio].pulse_count = 0;
-      }
-      if (l_gpio_data[gpio].pulse_count == 0) {
-         l_gpio_data[gpio].first_tick = tick;
-      }
-      l_gpio_data[gpio].last_tick = tick;
-      l_gpio_data[gpio].pulse_count++;
-      printf("logic level change to low\n\tcount: %d\n\ttime: %d\n", l_gpio_data[gpio].pulse_count, l_gpio_data[gpio].last_tick - l_gpio_data[gpio].first_tick);
-   }
+   if (level == 1) l_gpio_data[gpio].pulse_count++;
 
    if (g_reset_counts[gpio])
    {
@@ -163,43 +197,43 @@ int main(int argc, char *argv[])
    gpioPulse_t pulse[2];
    int count[MAX_GPIOS];
 
+   /* command line parameters */
+
+   rest = initOpts(argc, argv);
+
    /* get the gpios to monitor */
 
    g_num_gpios = 0;
 
-   for (i = 1; i < argc; i++)
+   for (i=rest; i<argc; i++)
    {
       g = atoi(argv[i]);
-      if ((g >= 0) && (g < 32))
+      if ((g>=0) && (g<32))
       {
          g_gpio[g_num_gpios++] = g;
-         g_mask |= (1 << g);
+         g_mask |= (1<<g);
       }
-      else
-         fatal(1, "%d is not a valid g_gpio number\n", g);
+      else fatal(1, "%d is not a valid g_gpio number\n", g);
    }
 
-   if (!g_num_gpios)
-      fatal(1, "At least one gpio must be specified");
+   if (!g_num_gpios) fatal(1, "At least one gpio must be specified");
 
    printf("Monitoring gpios");
-   for (i = 0; i < g_num_gpios; i++)
-      printf(" %d", g_gpio[i]);
+   for (i=0; i<g_num_gpios; i++) printf(" %d", g_gpio[i]);
    printf("\nSample rate %d micros, refresh rate %d deciseconds\n",
-          g_opt_s, g_opt_r);
+      g_opt_s, g_opt_r);
 
    gpioCfgClock(g_opt_s, 1, 1);
 
-   if (gpioInitialise() < 0)
-      return 1;
+   if (gpioInitialise()<0) return 1;
 
    gpioWaveClear();
 
-   pulse[0].gpioOn = g_mask;
+   pulse[0].gpioOn  = g_mask;
    pulse[0].gpioOff = 0;
    pulse[0].usDelay = g_opt_p;
 
-   pulse[1].gpioOn = 0;
+   pulse[1].gpioOn  = 0;
    pulse[1].gpioOff = g_mask;
    pulse[1].usDelay = g_opt_p;
 
@@ -209,8 +243,7 @@ int main(int argc, char *argv[])
 
    /* monitor g_gpio level changes */
 
-   for (i = 0; i < g_num_gpios; i++)
-      gpioSetAlertFunc(g_gpio[i], edges);
+   for (i=0; i<g_num_gpios; i++) gpioSetAlertFunc(g_gpio[i], edges);
 
    mode = PI_INPUT;
 
@@ -220,28 +253,28 @@ int main(int argc, char *argv[])
       mode = PI_OUTPUT;
    }
 
-   for (i = 0; i < g_num_gpios; i++)
-      gpioSetMode(g_gpio[i], mode);
+   for (i=0; i<g_num_gpios; i++) gpioSetMode(g_gpio[i], mode);
 
    while (1)
    {
       gpioDelay(g_opt_r * 100000);
 
-      for (i = 0; i < g_num_gpios; i++)
+      for (i=0; i<g_num_gpios; i++)
       {
-         // g = g_gpio[i];
-         // g_gpio_data[g] = l_gpio_data[g];
+         g = g_gpio[i];
+         g_gpio_data[g] = l_gpio_data[g];
 
-         // diff = g_gpio_data[g].last_tick - g_gpio_data[g].first_tick;
-         // tally = g_gpio_data[g].pulse_count;
-         // if (diff == 0)
-         //    diff = 1;
-         // printf("resetting\n");
-         // g_reset_counts[g] = 1;
-         // printf("g=%d %.0f (%d/%d)\n",
-         //        g, 1000000.0 * tally / diff, tally, diff);
+         diff = g_gpio_data[g].last_tick - g_gpio_data[g].first_tick;
+         tally = g_gpio_data[g].pulse_count;
+         if (diff == 0) diff = 1;
+
+         g_reset_counts[g] = 1;
+         printf("g=%d %.0f (%d/%d)\n",
+            g, 1000000.0 * tally / diff, tally, diff);
       }
+
    }
 
    gpioTerminate();
 }
+
